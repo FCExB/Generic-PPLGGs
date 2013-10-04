f1fb55965c61d95ef62384d2bf5250c91da9e376
Finlay Curran <fgc12@imperial.ac.uk>
2013-10-04 17:43:13 +0100

    Update image location references

--- OpenDoors/src/game/Program.java
+++ OpenDoors/src/game/Program.java
@@ -7,8 +7,8 @@ public class Program {
 	public static void main(String[] args){
 		PPLGGFront<Map> front = new PPLGGFront<Map>(new OpenDoorsAI(), new MapConverter(), new OpenDoors(), 180,14);
 		
-		//front.generatePLGs(180,14, 15, 25, 7, 20);
-		front.runPPLGGGUI();
+		front.generatePLGs(180,14, 15, 25, 7, 20);
+		//front.runPPLGGGUI();
 	}
 
 }
